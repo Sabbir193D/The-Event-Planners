acbf6eecf0eb092143e97a48f586e0e9aec7ee31
Sabbir Hossen <137142428+Sabbir193D@users.noreply.github.com>
2023-06-28 02:30:03 +0600

    Create TheEventPlanners.c

---
+++ TheEventPlanners.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+ struct EventRegistration{
+    char AttendeeName[50];
+    char AttendeeAddress[50];
+    char Contact[50];
+ }e;
+   struct Catering{
+       char Menu[50];
+       char Quantity[50];
+}d;
+     FILE *fp;
