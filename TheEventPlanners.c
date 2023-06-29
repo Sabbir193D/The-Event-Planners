854248ebcc369db553eaa4750a59c6bd0e051fb3
Sabbir Hossen <137142428+Sabbir193D@users.noreply.github.com>
2023-06-29 20:29:34 +0600

    Update TheEventPlanners.c

--- TheEventPlanners.c
+++ TheEventPlanners.c
@@ -11,4 +11,23 @@
        char Menu[50];
        char Quantity[50];
 }d;
+    FILE *fp;
+     void WelcomeScreen()
+{
+    printf("\n\n\n\n\n\t\t\t\t*****************************************");
+    printf("\n\t\t\t\t*\t\tWELCOME TO    \t\t*");
+    printf("\n\t\t\t\t*          THE EVENT PLANNERS           *  ");
+    printf("\n\t\t\t\t*****************************************");
+    printf("\n\n\n\nPlan your own Event.>.>.>.>.>.>\n");
+    getch();
+    system("cls");
+}
+
+void Headline()
+{
+    printf("\n\n\t\t\t-------------------------------------------------------------------------------");
+    printf("\n\t\t\t|                              THE EVENT PLANNERS                             |");
+    printf("\n\t\t\t-------------------------------------------------------------------------------");
+}
+
      
